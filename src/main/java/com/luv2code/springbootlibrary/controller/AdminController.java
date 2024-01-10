b214dbdd33eafd6b16688c8663c4313f5e37b925
LeSean Bruneau <ustbrun9912@gmail.com>
2024-01-10 07:57:30 -0600

    Section 30 - Admin - Change Quantity and Delete Books

--- src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -6,6 +6,8 @@ import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/admin")
@@ -18,6 +20,26 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorizaiton") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value="Authorizaiton") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value="Authorization") String token,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
@@ -26,6 +48,15 @@ public class AdminController {
             throw new Exception("Administration page only");
         }
         adminService.postBook(addBookRequest);
+    }
 
+    @DeleteMapping("/secure/delete/book")
+    public void deleteBook(@RequestHeader(value = "Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.deleteBook(bookId);
     }
 }
--- src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
@@ -4,11 +4,19 @@ import com.luv2code.springbootlibrary.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.repository.query.Param;
+
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 
     Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    @Modifying
+    @Query(value = "delete from review where book_id in :book_id", nativeQuery = true)
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
